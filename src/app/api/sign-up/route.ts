a2c1e109a1feb4a270ac575bd9fae6e8d1ae7828
Yogita Shete <yogitashete24@gmail.com>
2025-01-01 11:43:08 +0530

    Fix import path for sendVerificationEmail helper in sign-up route

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,7 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from "bcrypt";
 
-import { sendVerificationEmail } from "@/helper/sendVerificationEmail";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 
 export async function POST(request: Request) {
     await dbConnect();
